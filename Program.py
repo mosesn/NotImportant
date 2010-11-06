72a247f6ecbded818fe2672a1efc5686148ffbfe
unknown <Mark@.(none)>
2010-11-05 23:41:11 -0400

    Yo!

---
+++ Program.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width=500, height=500)
+canvas.pack()
+
+root.mainloop()
---
+++ View.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+location = [0,0]
+
+root = Tk()
+
+canvas = Canvas(root, width=800, height=800)
+canvas.pack()
+
