653ff9d4c9b91894612ced113927d05caa68bb82
Moses Nakamura <Moses@Hiro.Nakamura>
2010-11-06 03:44:01 -0400

    replaced view.py w/ program.py

--- View.py
+++ View.py
@@ -1,9 +1,31 @@
 from tkinter import *
 
-location = [0,0]
-
 root = Tk()
-
-canvas = Canvas(root, width=800, height=800)
+canvas = Canvas(root, width=500, height=500)
 canvas.pack()
 
+def showPosEvent(event):
+    print('Widget=%s X=%s Y=%s' % (event.widget, event.x, event.y))
+
+def onUpArrowKey(event):
+    print('Got up arrow key press')
+
+def onLeftArrowKey(event):
+    print('Got left arrow key press')
+
+def onRightArrowKey(event):
+    print('Got right arrow key press')
+
+def onDownArrowKey(event):
+    print('Got down arrow key press')
+
+widget = Label(root, text='')
+widget.pack(expand=YES, fill=BOTH)
+widget.bind('<Up>',onUpArrowKey)
+widget.bind('<Down>',onDownArrowKey)
+widget.bind('<Right>',onRightArrowKey)
+widget.bind('<Left>',onLeftArrowKey)
+
+widget.focus()
+
+root.mainloop()
